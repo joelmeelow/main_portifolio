20ce4575a6867f430488eb67fc1653ab3fc6ad60
joelmeelow <charles.ebuka90@gmail.com>
2024-11-02 14:54:01 +0100

    first commit

---
+++ main/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField(upload_to='projects/')
+    link = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.title
